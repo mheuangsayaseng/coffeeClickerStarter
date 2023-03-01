8a199411f35261164e2ae04542ee2e4f8cea738a
mheuangsayaseng <maggieheuang@gmail.com>
2023-03-01 01:28:48 -0600

    css/script solutions

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 // This line allows us to bring in the data object from our data.js file
+
 const data = window.data;
 
-// Make your References to the two DOM nodes
-
 // Create a reference to the element who's ID is 'big_coffee and call it bigCoffee
+const bigCoffee = document.querySelector("#big_coffee");
 
 // Create a reference to the element who's ID is 'producer_container' and call it producerContainer
+const producerContainer = document.querySelector("#producer_container");
 
 /**************
  *   SLICE 1
  **************/
 
 function updateCoffeeView(coffeeQty) {
+
   // Create a reference to the element who's ID is 'coffee_counter'
+  const coffeeCounter = document.querySelector("#coffee_counter")
+ 
   // Set the innerText of that element to be the coffeeQty passed into this function
+  coffeeCounter.innerText = coffeeQty
 }
 
+
 function clickCoffee(data) {
+
   // Increment the data object's (passed into this function) coffee property by one
+  data.coffee++;
+
   // call the updateCoffeeView function and pass it the newly updated data.coffee property
+  updateCoffeeView(data.coffee);
+
   // call the renderProducers function and pass it the data object
+  renderProducers(data);
+
 }
 
 /**************
@@ -27,15 +40,30 @@ function clickCoffee(data) {
  **************/
 
 function unlockProducers(producers, coffeeCount) {
+
   // loop through the producers array passed into the function
   // for each producer, if the coffeeCount (passed in) is greater than or equal
   // to half the producer's price, reassign the producers.unlocked property to equal true
+
+  for(const producer of producers) {
+    if(coffeeCount >= producer.price /2) {
+      producer.unlocked = true;
+    }
+  }
+
 }
 
 function getUnlockedProducers(data) {
+
   // use the Array.prototype.filter() method
   // filter through the data.producers property, and return an array with only the producers whose
   // unlocked property is true
+
+  const filteredArray = data.producers.filter((producer) => {
+    return producer.unlocked;
+  })
+  return filteredArray;
+  
 }
 
 // You do not need to edit this function
@@ -76,10 +104,13 @@ function deleteAllChildNodes(parent) {
 
 function renderProducers(data) {
   // call the unlockProducers function and pass it data.producers and data.coffee
+  unlockProducers(data.producers, data.coffee);
 
   // make a reference to the DOM element whose ID is producer_container
+  const producerContainer = document.getElementById("producer_container");
 
   // call the deleteAllChildNodes function and pass it the above producerContainer element
+  deleteAllChildNodes(producerContainer);
 
   // you do not need to edit the following code, but for understanding, this gets the unlocked producers,
   // and for each producer makes a little html div with that producer's info
@@ -145,8 +176,10 @@ function buyButtonClick(event, data) {
 function tick(data) {
   // increment the data object's (passed into this function)
   // coffee property by the data.totalCPS amount
+  data.coffee += data.totalCPS;
 
   // call the updateCoffeeView function and pass it the data.coffee property
+  updateCoffeeView(data.coffee);
 
   // call the renderProducers function and pass it the newly updated data object
   renderProducers(data);
@@ -156,9 +189,15 @@ function tick(data) {
 
 // add a 'click' event listener to the bigCoffee element (that you referenced above)
 // the event listener should call the clickCoffee function, and pass in the global data object
+bigCoffee.addEventListener("click",() => {
+  clickCoffee(data);
+})
 
 // add a 'click' event listener to the element (referenced at the top of the file)
 // the event listener should call the buyButtonClick function and pass it the event, and the global data object
+producerContainer/addEventListener("click", () => {
+  buyButtonClick(event, data);
+})
 
 // You do not need to edit this last line. This simple runs your tick function every 1000ms, or 1s
 setInterval(() => tick(data), 1000);
